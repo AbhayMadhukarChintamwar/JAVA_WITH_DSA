78195dd51fd7e283a28a54959ab97abb984a583b
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-03-17 10:14:40 +0530

    Add  Java Basic file

---
+++ src/Java_Basic/Java_Basic.java
@@ -0,0 +1,16 @@
+package Java_Basic;
+
+public class Java_Basic {
+
+    public static void main(String[] args){
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b " + (a && b));
+        System.out.println("a || b " + (a || b));
+        System.out.println("!a " + (!a));
+
+        int marks = 32;
+        String res = (marks>34)?"Pass" :"Fail";
+        System.out.println(res);
+    }
+}

82588b7e782f531062eba9e1dd7716877f85fdac
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-03-26 18:54:11 +0530

    Conditional and Pattern Printing code

--- src/Pattern/Special_Triangles/RhombusPattern.java
+++ src/Pattern/Special_Triangles/RhombusPattern.java
@@ -15,28 +15,41 @@ public class RhombusPattern {
 
  */
 
-                Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
+//        for (int i = 1; i <=n ; i++) {
+//            for (int j = 1; j <=n ; j++) {
+//                if (j>=n+1-i){
+//                    System.out.print("* ");
+//                }else {
+//                    System.out.print("  ");
+//
+//                }
+//            }
+//            for (int j = 2; j <=n ; j++) {
+//                if (j<= n+1-i){
+//                    System.out.print("* ");
+//                }else {
+//                    System.out.print("  ");
+//
+//                }
+//
+//            }
+//            System.out.println();
+//        }
+
+
         for (int i = 1; i <=n ; i++) {
-            for (int j = 1; j <=n ; j++) {
-                if (j>=n+1-i){
-                    System.out.print("* ");
-                }else {
-                    System.out.print("  ");
+            for (int j = 1; j <=n-i ; j++) {
+                System.out.print(" " + " ");
 
-                }
             }
-            for (int j = 2; j <=n ; j++) {
-                if (j<= n+1-i){
-                    System.out.print("* ");
-                }else {
-                    System.out.print("  ");
-
-                }
-
+            for (int j = 1; j <=n ; j++) {
+                System.out.print("*"+ " ");
             }
             System.out.println();
         }
+
     }
 }

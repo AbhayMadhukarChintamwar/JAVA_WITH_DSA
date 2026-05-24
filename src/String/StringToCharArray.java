3cea2194d0ad3380fd173157141db14da8166031
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-05-24 08:40:22 +0530

    Add StringToCharArray.java implementation to convert a string to a character array and print it

---
+++ src/String/StringToCharArray.java
@@ -0,0 +1,11 @@
+package String;
+
+public class StringToCharArray {
+    public static void main(String[] args) {
+        String s = "Abhay";
+        char[] charArray = s.toCharArray();
+        for (char c : charArray) {
+            System.out.print(c + " ");
+        }
+    }
+}

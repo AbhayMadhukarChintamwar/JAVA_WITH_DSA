d6b0fb381546b3cce374e369d21931d397df7e11
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-05-24 10:51:32 +0530

    Sum Of All Sub Strings Of A Number

--- src/String/Print_All_Substring.java
+++ src/String/Print_All_Substring.java
@@ -3,16 +3,13 @@ package String;
 public class Print_All_Substring {
     public static void main(String[] args) {
         String a = "Abhay";
-
-        int s = a.length();
-        for (int i = 0; i <= a.length(); i++) {
+        for (int i = 0; i < a.length(); i++) {
             for (int j = i + 1; j <= a.length(); j++) {
                 System.out.print(a.substring(i, j) +" ");
 
             }
             System.out.println();
         }
-
     }
 
 }
---
+++ src/String/Sum_Of_All_Sub_Strings_Of_A_Number.java
@@ -0,0 +1,22 @@
+package String;
+
+public class Sum_Of_All_Sub_Strings_Of_A_Number {
+    public static void main(String[] args) {
+        String str = "123";
+        int sum = 0;
+        int  n = str.length();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j <=n; j++) {
+                String sub = str.substring(i,j);
+                sum += Integer.parseInt(sub);
+            }
+            
+        }
+        System.out.println(sum);
+    }
+}
+
+
+// gfg practice problem
+// https://www.geeksforgeeks.org/problems/sum-of-all-substrings-of-a-number-1587115621/1

812afaaeb6936d71c0940d496ee69baed412c72f
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-05-24 08:57:26 +0530

    Add SubStrings.java implementation to demonstrate string manipulation and substring extraction

---
+++ src/String/SubStrings.java
@@ -0,0 +1,15 @@
+package String;
+
+public class SubStrings {
+    public static void main(String[] args) {
+        String s = "Abhay";
+      System.out.println(s);
+    //   s.charAt(0) = 'a'; // This will give an error because strings are immutable in Java...this is not possible
+    //   System.out.println(s);
+        
+       System.out.println(s.substring(0, 3)); // This will print the substring from index 0 to index 2 (3-1)  //Abh
+       System.out.println(s.substring(3, 5)); // This will print the substring from index 3 to index 4 (5-1)  // ay
+       System.out.println(s.substring(2)); // This will print the substring from index 2 to the end of the string  // hay
+
+    }
+}
